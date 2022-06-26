42642b3327f0078524048b150783013f03bba1ea
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-06-26 23:58:19 +0200

    initial commit

---
+++ src/test/java/com/example/profileapi/ProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
